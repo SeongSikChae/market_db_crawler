30cbb5156c1add55a21535f1a26b5f5786af337a
ChaeSeongSik <css5831@outlook.com>
2024-05-02 13:06:06 +0900

    CrawlerProvider 인터페이스 추가

---
+++ MarketCrawlerLib/Crawler/ICrawlerProvider.cs
@@ -0,0 +1,35 @@
+﻿namespace MarketCrawlerLib.Crawler
+{
+    public enum CrawlerType
+    {
+        Auction, Coupang, GMarket, Naver, Street11
+    }
+
+    public interface ICrawlerProvider
+    {
+        ICrawler GetCrawler(CrawlerType type);
+
+        static ICrawlerProvider Default { get; } = new DefaultCrawlerProvider();
+
+        private sealed class DefaultCrawlerProvider : ICrawlerProvider
+        {
+            public DefaultCrawlerProvider()
+            {
+                crawlerDic.Add(CrawlerType.Auction, new AuctionCrawler());
+                crawlerDic.Add(CrawlerType.Coupang, new CoupangCrawler());
+                crawlerDic.Add(CrawlerType.GMarket, new GMarketCrawler());
+                crawlerDic.Add(CrawlerType.Naver, new NaverCrawler());
+                crawlerDic.Add(CrawlerType.Street11, new Street11Crawler());
+            }
+
+            private readonly Dictionary<CrawlerType, ICrawler> crawlerDic = new Dictionary<CrawlerType, ICrawler>();
+
+            public ICrawler GetCrawler(CrawlerType type)
+            {
+                if (crawlerDic.TryGetValue(type, out ICrawler? crawler))
+                    return crawler;
+                throw new InvalidOperationException($"not found crawler : {type}");
+            }
+        }
+    }
+}
---
+++ MarketCrawlerLibTests/Crawler/ICrawlerProviderTests.cs
@@ -0,0 +1,104 @@
+﻿using MarketCrawlerLib.Crawler;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketCrawlerLibTests.Crawler
+{
+    [TestClass]
+    public class ICrawlerProviderTests
+    {
+        [TestMethod]
+        public async Task GetAuctionCrawlerTest()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            ICrawler crawler = ICrawlerProvider.Default.GetCrawler(CrawlerType.Auction);
+            List<Category> categories = await crawler.GetCategories();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                await System.Text.Json.JsonSerializer.SerializeAsync(stream, categories, new System.Text.Json.JsonSerializerOptions
+                {
+                    Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+                });
+                stream.Position = 0;
+                using StreamReader reader = new StreamReader(stream);
+                Trace.WriteLine(reader.ReadToEnd());
+            }
+        }
+
+        [TestMethod]
+        public async Task GetCoupangCrawlerTest()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            ICrawler crawler = ICrawlerProvider.Default.GetCrawler(CrawlerType.Coupang);
+            List<Category> categories = await crawler.GetCategories();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                await System.Text.Json.JsonSerializer.SerializeAsync(stream, categories, new System.Text.Json.JsonSerializerOptions
+                {
+                    Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+                });
+                stream.Position = 0;
+                using StreamReader reader = new StreamReader(stream);
+                Trace.WriteLine(reader.ReadToEnd());
+            }
+        }
+
+        [TestMethod]
+        public async Task GetGMarketCrawlerTest()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            ICrawler crawler = ICrawlerProvider.Default.GetCrawler(CrawlerType.GMarket);
+            List<Category> categories = await crawler.GetCategories();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                await System.Text.Json.JsonSerializer.SerializeAsync(stream, categories, new System.Text.Json.JsonSerializerOptions
+                {
+                    Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+                });
+                stream.Position = 0;
+                using StreamReader reader = new StreamReader(stream);
+                Trace.WriteLine(reader.ReadToEnd());
+            }
+        }
+
+        [TestMethod]
+        public async Task GetNaverCrawlerTest()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            ICrawler crawler = ICrawlerProvider.Default.GetCrawler(CrawlerType.Naver);
+            List<Category> categories = await crawler.GetCategories();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                await System.Text.Json.JsonSerializer.SerializeAsync(stream, categories, new System.Text.Json.JsonSerializerOptions
+                {
+                    Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+                });
+                stream.Position = 0;
+                using StreamReader reader = new StreamReader(stream);
+                Trace.WriteLine(reader.ReadToEnd());
+            }
+        }
+
+        [TestMethod]
+        public async Task GetStreet11CrawlerTest()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            ICrawler crawler = ICrawlerProvider.Default.GetCrawler(CrawlerType.Street11);
+            List<Category> categories = await crawler.GetCategories();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                await System.Text.Json.JsonSerializer.SerializeAsync(stream, categories, new System.Text.Json.JsonSerializerOptions
+                {
+                    Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+                });
+                stream.Position = 0;
+                using StreamReader reader = new StreamReader(stream);
+                Trace.WriteLine(reader.ReadToEnd());
+            }
+        }
+    }
+}
